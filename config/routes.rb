385118c159d1d997300157d67493f785aed99b69
robert haynes <roberthaynes@roberts-MacBook.local>
2018-08-10 12:54:39 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :home, only: [:index]
+  resources :projects, only: [:index, :show]
+  resources :about, only: [:index]
+  resources :contact, only: [:index]
 end
---
+++ db/migrate/20180809180310_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false
+      t.string :url, null: false, length: 1000
+      t.string :github, null: false, length: 1000
+
+      t.timestamps
+    end
+  end
+end
